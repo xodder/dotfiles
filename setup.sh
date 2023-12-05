bb3ec9f2f6107b59ca46eafcb69bdc8a2ca25484
Stephen Odebiyi <odebiyistephen@gmail.com>
2023-12-05 14:10:54 +0100

    Update setup script

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/zsh
 
 repo="https://github.com/xodder/dotfiles.git"
 destination="$HOME/.dotfiles"
@@ -11,16 +11,20 @@ echo "[INFO] Setting up dotfiles..."
 git clone "$repo" "$destination"
 
 # copy in our zshrc addon
-cp -fq "$destination/refs/.zshrc-addons" "$HOME"
+cp -f "$destination/refs/.zshrc-addons" "$HOME"
 
 # is it already injected?
-already_injected=$(cat .zshrc | grep -cq ".zshrc-addons")
+already_injected=$(cat .zshrc | grep -c ".zshrc-addons")
 
 # inject into current zshrc
-if [ $already_injected -eq 0 ]; then
+if [[ $already_injected -eq 0 ]]; then
+  echo "" >> .zshrc
   echo "# load extras" >> .zshrc
   echo "[ -s \"$HOME/.zshrc-addons\" ] && \. \"$HOME/.zshrc-addons\"" >> .zshrc
 fi
+
+# reload zshrc
+source .zshrc
 
 echo "[INFO] Setup complete"
 echo "[INFO] To get started, run 'dotfiles install'"
